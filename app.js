d394a9d513fd294559b4bc50231b9d0859924a5a
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-01-01 14:27:45 -0500

    Countdown exercise complete

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("I am connected!")
+// Countdown exercise
+
+const countdown = (num) => {
+  const stopInterval = setInterval(function () {
+    // if num less than zero return nothing
+    if (num < 0) return;
+    // once num reaches zero returns "DONE" and stops the interval from continuing
+    if (num === 0) {
+      console.log("DONE!");
+      clearInterval(stopInterval);
+    } else {
+      // Continue countdown with number being one less until reach zero
+      console.log(num);
+      num -= 1;
+    }
+  }, 1000);
+};
+
+countdown(3);
